10c47563b2f64d4717c588b2caf0dfe3cecce160
Ankit Kumar <ankitkumarswe01@gmail.com>
2026-07-09 10:19:05 +0530

    module

---
+++ module.js
@@ -0,0 +1,19 @@
+//module wrapper
+
+console.log(`Filename: ${__filename}`);
+console.log(`Dirname: ${__dirname}`);
+console.log(`Require: ${require}`);
+console.log(`Module: ${module}`);
+console.log(`Exports: ${exports}`);
+
+
+
+
+
+
+
+
+
+
+
+
