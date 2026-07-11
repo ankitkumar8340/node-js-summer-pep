3bb87f224f2f7a2a4b3a58ba23a5102dc25de93b
Ankit Kumar <ankitkumarswe01@gmail.com>
2026-07-11 21:18:21 +0530

    multer storage

--- express/file_op.js
+++ express/file_op.js
@@ -13,3 +13,15 @@ if (!fs.existsSync("uploads")) {
     fs.mkdirSync("uploads");
 }
 
+// Multer Storage
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "uploads/");
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + "-" + file.originalname);
+    }
+});
+
+const upload = multer({ storage });
+
